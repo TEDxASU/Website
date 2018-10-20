627df96144e8119c5cad18c0cbceb0974ebd0214
Alireza Bahremand <abahrema@asu.edu>
2018-10-20 00:25:48 -0700

    Landing Page
    Replica of landing page reflecting prior wireframes.

---
+++ assets/js/actions.js
@@ -0,0 +1,13 @@
+window.onscroll = function() {
+  growShrinkLogo()
+};
+
+function growShrinkLogo() {
+  // Grab navbar logo and shrink logo on scroll.
+  var Logo = document.getElementById("imgNavBarLogo")
+  if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+    Logo.style.width = '14%';
+  } else {
+    Logo.style.width = '18%';
+  }
+}
