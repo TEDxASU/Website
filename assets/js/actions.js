63832a83829a6ad9338d7b35372f7b6360d4d0a2
Alireza Bahremand <abahrema@asu.edu>
2018-10-27 22:05:59 -0700

    Parallax, additional JS functionality, and fullscreen images.

--- assets/js/actions.js
+++ assets/js/actions.js
@@ -1,23 +1,70 @@
+// TODO: Create respective function for lifecycle hook calls.
+
 window.onscroll = function() {
   growShrinkLogo()
 };
 
+// Check for window resize, and rescale parallax.
+$(window).resize(function() {
+  $('.parallax-container').css({'height': $(window).height() + 'px',
+  'width' : $(window).width() + 'px'});
+  $('.parallax').parallax();
+});
+
 function growShrinkLogo() {
   // Grab navbar logo and shrink logo on scroll.
   var Logo = document.getElementById("imgNavBarLogo")
+  var navbarBackground = document.getElementsByClassName('nav');
   if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
     Logo.style.width = '14%';
+    navbarBackground.style.height='12%';
   } else {
     Logo.style.width = '18%';
+    navbarBackground.style.height='16%';
   }
 }
 
+// Jumbotron function to move image caption near middle
+// without slider indicators for a autoplay of image browsing.
 $(document).ready(function(){
   $('.slider').slider({
     indicators: false,
     height: 800
   });
 });
+
+$(document).ready(function(){
+  $('.parallax-container').css({'height': $(window).height() + 'px',
+  'width' : $(window).width() + 'px'});
+  $('.parallax').parallax();
+});
+
+$(document).load(function(){
+  // $('.parallax-container').trigger('scroll');
+  // $('.parallax').trigger('scroll');
+});
+
+$(document).ready(function(){
+  // hide .navbar first
+  $(".tedx-nav").hide();
+  // fade in .navbar
+  $(function () {
+      $(window).scroll(function () {
+          // set distance user scrolls before fadeIn starts
+          if ($(this).scrollTop() > 100) {
+              $("#tedx-navbar").fadeIn();
+          } else {
+              $("#tedx-navbar").fadeOut();
+          }
+      });});
+});
+
+$(document).ready(function(){
+  $('.modal').modal();
+  $('.dropdown-trigger').dropdown();
+  $('select').not('.disabled').formSelect();
+});
+
 
 $(document).ready(function(){
   $('.collapsible').collapsible();
